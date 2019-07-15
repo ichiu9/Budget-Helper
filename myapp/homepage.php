39db1929af4865d8efcc61a3f18f95a360e7a8e9
ichiu9 <43252713+ichiu9@users.noreply.github.com>
2019-07-15 12:10:04 -0700

    Updated Title

--- myapp/homepage.php
+++ myapp/homepage.php
@@ -14,7 +14,7 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/css/bootstrap-datepicker3.css"/>
 
 
-<title>jQuery UI Datepicker - Default functionality</title>
+<title>50/30/20 Budget-Helper</title>
   <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
   <link rel="stylesheet" href="/resources/demos/style.css">
   <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
@@ -25,104 +25,175 @@
   } );
   </script>
 
+
+<!--- CSS STYLE -->
 <head>
-<style>
-body {
-  font-family: "Lato", sans-serif;
-}
-table.table1 {border-collapse: collapse; width: 80%;}
-table.table1 th.table1 {background-color: #4CAF50; padding: 8px; color: white;font-size:14px}
-table.table1 td.table1 { width:20%; padding: 8px; font-size: 12px}
-table.table1 tr.table1:nth-child(even){background-color: #f2f2f2;}
+  <style>
+  body {
+    font-family: "Lato", sans-serif;
+  }
+  table.table1 {border-collapse: collapse; width: 80%;}
+  table.table1 th.table1 {background-color: #4CAF50; padding: 8px; color: white;font-size:14px}
+  table.table1 td.table1 { width:20%; padding: 8px; font-size: 12px}
+  table.table1 tr.table1:nth-child(even){background-color: #f2f2f2;}
 
-table.table2 {border-collapse: collapse; width: 80%;}
-table.table2 th.table2 {background-color: #808080; padding: 8px; color: white; font-size: 20px}
-table.table2 td.table2 {text-align: left; padding: 8px}
-table.table2 tr.table2:nth-child(even){background-color: #008080;}
+  table.table2 {border-collapse: collapse; width: 80%;}
+  table.table2 th.table2 {background-color: #808080; padding: 8px; color: white; font-size: 20px}
+  table.table2 td.table2 {text-align: left; padding: 8px}
+  table.table2 tr.table2:nth-child(even){background-color: #008080;}
 
-.button {
-  background-color: #4CAF50;
-  border: none;
+  .button {
+    background-color: #4CAF50;
+    border: none;
+    color: white;
+    padding: 1px 10px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 20px;
+    border: 1px black;
+    margin: 4px 2px;
+    cursor: pointer;
+  }
+
+  .sidenav {height: 100%; 
+    width: 20%; 
+    position: fixed; 
+    z-index: 1; 
+    top:15%;
+    right: 0;
+    background-color: white;
+    overflow-x: hidden;
+    color: gray;
+  }
+
+  /* Style the sidenav links and the dropdown button */
+  .sidenav a, .dropdown-btn {
+    padding: 6px 8px 6px 16px;
+    text-decoration: none;
+    font-size: 15px;
+    color: #818181;
+    display: block;
+    border: none;
+    background: none;
+    width: 100%;
+    text-align: left;
+    cursor: pointer;
+    padding-top: 10px;
+    padding-bottom: 10px;
+    outline: none;
+  }
+
+  /* On mouse-over */
+  .sidenav a:hover, .dropdown-btn:hover {
+    color: black;
+  }
+
+  /* Main content */
+  .main {
+    margin-left: 200px; /* Same as the width of the sidenav */
+    font-size: 20px; /* Increased text to enable scrolling */
+    padding: 0px 10px;
+  }
+
+  /* Add an active class to the active dropdown button */
+  .active {
+    background-color: green;
+    color: white;
+  }
+
+  /* Dropdown container (hidden by default). Optional: add a lighter background color and some left padding to change the design of the dropdown content */
+  .dropdown-container {
+    display: none;
+    background-color: #262626;
+    padding-left: 8px;
+  }
+
+  .open-button {
+  background-color: #555;
   color: white;
-  padding: 15px 32px;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-  font-size: 16px;
-  margin: 4px 2px;
-  cursor: pointer;
-}
-
-.sidenav {height: 100%; 
-  width: 21%; 
-  position: fixed; 
-  z-index: 1; 
-  top: 80px; 
-  right: 0;
-  background-color: white;
-  overflow-x: hidden;
-  color: gray;
-}
-
-/* Style the sidenav links and the dropdown button */
-.sidenav a, .dropdown-btn {
-  padding: 6px 8px 6px 16px;
-  text-decoration: none;
-  font-size: 15px;
-  color: #818181;
-  display: block;
+  padding: 16px 20px;
   border: none;
-  background: none;
-  width: 100%;
-  text-align: left;
   cursor: pointer;
-  padding-top: 10px;
-  padding-bottom: 10px;
+  opacity: 0.8;
+  position: fixed;
+  bottom: 23px;
+  right: 28px;
+  width: 280px;
+}
+
+/* The popup form - hidden by default */
+.form-popup {
+  background-color: white;
+  display: none;
+  position: fixed;
+  right: 15px;
+  border: 3px solid #f1f1f1;
+  color: Black;
+  z-index: 9;
+}
+
+/* Add styles to the form container */
+.form-container {
+  max-width: 300px;
+  padding: 10px;
+  background-color: white;
+}
+
+/* Full-width input fields */
+.form-container input[type=text], .form-container input[type=password] {
+  width: 100%;
+  padding: 15px;
+  margin: 5px 0 22px 0;
+  border: none;
+  background: #f1f1f1;
+}
+
+/* When the inputs get focus, do something */
+.form-container input[type=text]:focus, .form-container input[type=password]:focus {
+  background-color: #ddd;
   outline: none;
 }
 
-/* On mouse-over */
-.sidenav a:hover, .dropdown-btn:hover {
-  color: black;
-}
-
-/* Main content */
-.main {
-  margin-left: 200px; /* Same as the width of the sidenav */
-  font-size: 20px; /* Increased text to enable scrolling */
-  padding: 0px 10px;
-}
-
-/* Add an active class to the active dropdown button */
-.active {
-  background-color: green;
+/* Set a style for the submit/login button */
+.form-container .btn {
+  background-color: #4CAF50;
   color: white;
+  padding: 16px 20px;
+  border: none;
+  cursor: pointer;
+  width: 100%;
+  margin-bottom:10px;
+  opacity: 0.8;
 }
 
-/* Dropdown container (hidden by default). Optional: add a lighter background color and some left padding to change the design of the dropdown content */
-.dropdown-container {
-  display: none;
-  background-color: #262626;
-  padding-left: 8px;
+/* Add a red background color to the cancel button */
+.form-container .cancel {
+  background-color: red;
 }
 
-/* Optional: Style the caret down icon */
-.fa-caret-down {
-  float: right;
-  padding-right: 8px;
+/* Add some hover effects to buttons */
+.form-container .btn:hover, .open-button:hover {
+  opacity: 2;
 }
 
-.main {
-  margin-left: 200px; /* Same as the width of the sidenav */
-  font-size: 20px; /* Increased text to enable scrolling */
-  padding: 0px 10px;
-}
+  /* Optional: Style the caret down icon */
+  .fa-caret-down {
+    float: right;
+    padding-right: 8px;
+  }
 
-@media screen and (max-height: 450px) {
-  .sidenav {padding-top: 15px;}
-  .sidenav a {font-size: 18px;}
-}
-</style>
+  .main {
+    margin-left: 200px; /* Same as the width of the sidenav */
+    font-size: 20px; /* Increased text to enable scrolling */
+    padding: 0px 10px;
+  }
+
+  @media screen and (max-height: 450px) {
+    .sidenav {padding-top: 15px;}
+    .sidenav a {font-size: 18px;}
+  }
+  </style>
 
 </head>
 <body>
@@ -132,23 +203,23 @@ $servername = "localhost";
 $username = "ichiu";
 $password = "Isaiahchiu9";
 
-//Connection and SQL Files
+//CONNECTION AND BASE SQL FILE UPLOAD
 try {
     $conn = new PDO("mysql:host=$servername;dbname=budget", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $conn->exec(file_get_contents('dropschema.sql'));
-    $conn->exec(file_get_contents('createschema.sql'));
-    $conn->exec(file_get_contents('populatesmall.sql'));
-    $conn->exec(file_get_contents('categ_spread_join.sql'));
+    #$conn->exec(file_get_contents('dropschema.sql'));
+    #$conn->exec(file_get_contents('createschema.sql'));
+    #$conn->exec(file_get_contents('populatesmall.sql'));
+    #$conn->exec(file_get_contents('categ_spread_join.sql'));
 
     $res = $conn->query('SELECT COUNT(*) FROM category');
     $num_rows = $res->fetchColumn();
 
     
 ?>
-
+<!--- SIDENAV--->
 <div class="sidenav">
   <button class="dropdown-btn">Add Living Expenses
     <i class="fa fa-caret-down"></i>
@@ -234,6 +305,7 @@ try {
   </div>
 </div>
 
+<!---Dropdown button Javascript file-->
 <script>
   var dropdown = document.getElementsByClassName("dropdown-btn");
   var i;
@@ -258,17 +330,20 @@ try {
     $type = $_POST['radbean'];
     $wedate = $_POST['date'];
     $date = date("Y-m-d", strtotime($wedate));
+  }else{
+    $name= "";
+    $cost= 0;
+    $type = "";
+    $date = "";
+  }
     echo $name, " - ", $cost, " - ",$type, " - ", $date;
-    echo "OK SO NEXT STEPS TO FINISH ARE: Implement add or delete subcategory feature and show purchase history";
-  } 
+    $sql_insert_purchase = "INSERT INTO purchase VALUES ('$name', '$cost', '$date');
+                            INSERT INTO made_of VALUES ('$name', '$date', '$type', '$date');
+                            UPDATE category SET total_money = total_money+'$cost' WHERE category_name='$type';
+                            ";
+    $result_purchase = $conn->query($sql_insert_purchase);
 
-  $sql_insert_purchase = "INSERT INTO purchase VALUES ('$name', $cost, '$date');
-              INSERT INTO made_of VALUES ('$name', '$date', '$type', '$date');
-              UPDATE category SET total_money = total_money+$cost WHERE category_name='$type';
-              ";
-  $result_purchase = $conn->query($sql_insert_purchase);
-
-
+  
   //Query 4 Query for Total headings from Account
   $sql4 = "SELECT allowance FROM spreadsheet";
   $result4 = $conn->query($sql4);
@@ -357,16 +432,57 @@ try {
                     <th class='table1'>$val ".$resultbean[0][$type]."%</th>
                     <th class='table1'></th>
                     <th class='table1'></th>
+                    <th class='table1'>
+                    <button class='button' onclick='openForm()'>+</button>  
+                    </th>
                     </tr>";
+                ?>
+        <!---!!!!!!!REEEEEE-->
+            <div class='form-popup' id='myForm'>
+            <form action ='add_or_delete.php' method='get'>                
+                <h1>Add Category</h1>
+                <input type='hidden' name='add_or_delete' value="+">
+                <input type="hidden" name="type" value="$val">
+                <label for="cat"><b>Category Name:</b></label>
+                <input type="text" name="cat" required>
+                <label for="desc"><b>Description</b></label>
+                <input type="text" name="desc">
+                <button type="submit" class="btn">Add</button>
+                <button type="button" class="btn cancel" onclick="closeForm()">Cancel</button>
+                </form>
+              </div>
+
+
+              <script>
+                function openForm() {
+                document.getElementById('myForm').style.display = 'block';
+                }
+
+                function closeForm() {
+                document.getElementById('myForm').style.display = 'none';
+                }
+                </script>
+        
+            <?php
+
             // Subcategories
+
             while($row = $result->fetch(PDO::FETCH_ASSOC)) {
                 echo "<tr class='table1'>
                 <td class='table1'></td>
                         <td class='table1'>".$row["total_money"]."</td>
                         <td class='table1'>".$row["category_name"]."</td>
                         <td class='table1'>".$row["description"]."</td>
-                        <td class='table1'></td></tr>";
-                        
+                        <td class='table1'>
+                        <td class='table1'>
+                          <div class='btn-group'>
+                            <form action ='add_or_delete.php' method='get'>
+                              <button class='button' name='add_or_delete' value = '-' onclick='add_or_delete.php'>-</button>
+                              <input type='hidden' name='category' value='".$row["category_name"]."'>
+                            </form>
+                          </div>
+                        </td>
+                      </tr>";         
             }
             echo "</table>";
         } else {
